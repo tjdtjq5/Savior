66002df0092fa64c4fe6075be24fc72e16e9b17a
tjdtjq5 <tjdtjq5@naver.com>
2020-07-01 22:54:59 +0900

    tjdtjq

--- Assets/Script/Player/PlayerController.cs
+++ Assets/Script/Player/PlayerController.cs
@@ -2,9 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using DG.Tweening;
 using Spine.Unity;
-using UnityEditor.ShaderGraph.Internal;
 
 public class PlayerController : MonoBehaviour
 {
